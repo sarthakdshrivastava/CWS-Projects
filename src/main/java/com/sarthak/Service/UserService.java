1e93713ff8508997915e8f8fc47aff74449b8e1e
tempacc2204 <sarthakshrivtab@gmail.com>
2016-10-25 16:09:35 +0530

    Wrote REST controller for web application

---
+++ src/main/java/com/sarthak/Service/UserService.java
@@ -0,0 +1,21 @@
+package com.sarthak.Service;
+
+import com.sarthak.DTO.UserDTO;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Sarthak on 25-10-2016.
+ */
+@RestController
+public class UserService {
+    @RequestMapping(value = "/authenticate",method = RequestMethod.POST)
+    public boolean authenticateUser(@RequestBody UserDTO userDTO){
+        if(userDTO.getName().equals("sarthak") && userDTO.getPassword().equals("abc123"))
+            return true;
+        else
+            return false;
+    }
+}
---
+++ src/main/java/com/sarthak/SprintRestApplication.java
@@ -0,0 +1,12 @@
+package com.sarthak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintRestApplication.class, args);
+	}
+}
